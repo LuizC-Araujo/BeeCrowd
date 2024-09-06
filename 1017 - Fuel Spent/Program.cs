787bdd332031b8d1cded68b0bf2110ae75e025e4
Luiz Araujo <arauldesign@gmail.com>
2024-09-06 12:30:40 -0300

    fuel spent problem

---
+++ 1017 - Fuel Spent/Program.cs
@@ -0,0 +1,8 @@
+﻿int spentTime = int.Parse(Console.ReadLine());
+int averageSpeed = int.Parse(Console.ReadLine());
+
+const double consume = 12.0;
+
+double liters = Math.Round(spentTime * averageSpeed / consume, 3);
+
+Console.WriteLine(liters.ToString("F3"));
