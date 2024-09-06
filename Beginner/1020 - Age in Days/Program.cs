7e7358d20aabe40081e753bb7692c536e3772335
Luiz Araujo <arauldesign@gmail.com>
2024-09-06 13:24:04 -0300

    Age in Days problem

---
+++ Beginner/1020 - Age in Days/Program.cs
@@ -0,0 +1,11 @@
+﻿int days = int.Parse(Console.ReadLine());
+
+int years = days / 365;
+days = days % 365;
+
+int months = days / 30;
+days = days % 30;
+
+Console.WriteLine($"{years} ano(s)"); 
+Console.WriteLine($"{months} mes(es)"); 
+Console.WriteLine($"{days} dia(s)"); 
