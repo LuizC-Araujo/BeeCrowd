ce281a6b60cef8425154cc54d72989e53f1bbef9
Luiz Araujo <arauldesign@gmail.com>
2024-09-06 12:50:48 -0300

    banknotes problem

---
+++ 1018 - Banknotes/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] ints = new int[7];
+int[] notas = new int[] { 100, 50, 20, 10, 5, 2, 1 };
+
+int number = int.Parse(Console.ReadLine());
+string givenNumber = number.ToString();
+
+for  (int i = 0, j = 0; i < ints.Length; i++, j++)
+{
+    ints[i] = number / notas[j];
+    number = number % notas[j];
+}
+
+Console.WriteLine(givenNumber);
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"{ints[i]} nota(s) de R$ {notas[i]},00");
+}
+
+
